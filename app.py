fd205d867efe1ca5f7ac5cf81d8b9c264a799d42
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-05-11 20:35:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
-
+import os
 import RecarregarBanco
 
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 5000))
+
+
 
 def my_task():
     # coloque o código que você deseja executar continuamente aqui
@@ -19,4 +23,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+
